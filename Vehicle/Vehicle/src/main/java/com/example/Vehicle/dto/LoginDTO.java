522431868f13bf2eec85b92f326950723379e3da
Himaka12 <himakauthpala14@gmail.com>
2026-04-05 10:10:55 +0530

    Login DTO creation

---
+++ Vehicle/Vehicle/src/main/java/com/example/Vehicle/dto/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.example.Vehicle.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Please enter a valid email address.")
+    private String email;
+
+    @NotBlank(message = "Password is required.")
+    private String password;
+}
