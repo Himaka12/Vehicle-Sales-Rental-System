a8566c5a178c99314eccffd7f362af7e746343de
Himaka12 <himakauthpala14@gmail.com>
2026-04-11 22:07:15 +0530

    User java create

---
+++ Vehicle/Vehicle/src/main/java/com/example/Vehicle/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.Vehicle.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    private String contactNumber;
+
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean isActive = true;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean isPremium = false;
+
+    private String cardNumber;
+}
