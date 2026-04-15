d4dbc86f68960b9fefab71ed10c73cb9641e94e4
Himaka12 <himakauthpala14@gmail.com>
2026-04-15 20:50:44 +0530

    vehicle dto

---
+++ Vehicle/Vehicle/src/main/java/com/example/Vehicle/dto/VehicleDTO.java
@@ -0,0 +1,31 @@
+package com.example.Vehicle.dto;
+
+import lombok.Data;
+
+@Data
+public class VehicleDTO {
+    private Long id;
+    private String listingType;
+    private String vehicleCondition;
+    private String brand;
+    private String model;
+    private String category;
+    private int manufactureYear;
+    private String color;
+    private String listedDate;
+    private int quantity;
+    private int mileage;
+    private String engineCapacity;
+    private String fuelType;
+    private String transmission;
+    private String description;
+    private double price;
+    private String status;
+    private String image1;
+    private String image2;
+    private String image3;
+    private String image4;
+    private String image5;
+    private double effectivePrice;
+    private AppliedPromotionDTO appliedPromotion;
+}
