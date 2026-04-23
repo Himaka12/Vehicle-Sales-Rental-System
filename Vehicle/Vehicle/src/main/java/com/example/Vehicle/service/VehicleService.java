9430f696e8c9923bf3c1f605b861c60d3d1377fb
Himaka12 <himakauthpala14@gmail.com>
2026-04-23 17:38:00 +0530

    Update Vehicle Service

--- Vehicle/Vehicle/src/main/java/com/example/Vehicle/service/VehicleService.java
+++ Vehicle/Vehicle/src/main/java/com/example/Vehicle/service/VehicleService.java
@@ -8,6 +8,9 @@ public interface VehicleService {
     VehicleDTO addVehicle(VehicleSubmitDTO submitDTO);
     List<VehicleDTO> getAllVehicles();
     VehicleDTO getVehicleById(Long id);
+    List<VehicleDTO> getAdminVehicles();
+    VehicleDTO getAdminVehicleById(Long id);
     VehicleDTO updateVehicle(Long id, VehicleSubmitDTO submitDTO);
+    VehicleDTO updateVehicleVisibility(Long id, boolean visible);
     void deleteVehicle(Long id);
 }
